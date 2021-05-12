31ecb5b4adeddf2092c3adccc948785b5c5e274b
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-05-12 17:48:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 resource "google_container_cluster" "primary" {
  
   name  = "${var.cls_name}"
-  location_id = "${var.location_id}"
+  location_id = "${var.cls_location_id}"
  # We can't create a cluster with no node pool defined, but we want to only use
  # separately managed node pools. So we create the smallest possible default
 #  node pool and immediately delete it.
