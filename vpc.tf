613e7f03f33a0af2da41e2ab364e35a90c3de40a
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-05-12 17:39:01 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,10 @@
 #   description = "region"
 # }
 
-provider "google" {
-  project = "${var.project_id}"
-  region  = "${var.region}"
-}
+# provider "google" {
+#   project = "${var.project_id}"
+#   region  = "${var.region}"
+# }
 
 # VPC
 resource "google_compute_network" "vpc" {
