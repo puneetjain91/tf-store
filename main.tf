e659b0df72db755b0e483f6be8678451842e042d
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-05-12 13:01:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,54 @@
+provider "google" {
+  version = "3.5.0"
 
+ # credentials = "${file("terrafrom.json")}"
+
+  #   project = "gcp-service-line"
+  #   region  = "us-central1"
+  project = ${var.project_id}
+  region  = ${var.region}
+}
+resource "google_container_cluster" "primary" {
+#   name     = "my-cluster"
+#   location = "us-central1"
+#   subnetwork = "default"
+  
+  cls_name  = ${var.cls_name}
+  location_id = "${var.location_id}"
+  subnetwork = "default"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+  remove_default_node_pool = true
+  initial_node_count    = "${var.initial_node_count}"
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+    #   name       = "my-node-pool"
+    #   location   = "us-central1"
+    #   cluster    = google_container_cluster.primary.name
+    #   node_count = 1
+  container_node_pool_name  =  "${var.container_node_pool_name}"
+  location_id = "${var.location_id}"
+  cluster    = "${var.google_container_cluster.primary.name}"
+  node_count = "{var.node_count}"
+  
+node_config {
+
+    # preemptible  = true 
+    # machine_type = "e2-medium"
+
+    preemptible  = true 
+    machine_type = "{var.machine_type}"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
